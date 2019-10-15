ec2ee65995e90b5b83e11f90f33fa25b414ac536
huangkangji <huangkangji@goodsogood.com>
2019-10-15 09:36:05 +0800

    init commit

---
+++ src/main/java/com/goodsogood/ows/OwsClickSpyApplication.java
@@ -0,0 +1,13 @@
+package com.goodsogood.ows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwsClickSpyApplication {
+
+     public static void main(String[] args) {
+          SpringApplication.run(OwsClickSpyApplication.class, args);
+     }
+
+}
